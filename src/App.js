786bfdea9a1dd89645dc44737849401032d459ab
SeattleDantz <tricia@curo-software.com>
2021-06-06 18:38:33 -0700

    Added Dictionary component with form to start search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <Dictionary />
+        <footer className="text-center App-footer">
+          <small>
+            <a href="https://github.com/SeattleDantz/react-dictionary-project">
+              Open source code
+            </a>{" "}
+            by Tricia Brigham
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
